f30f745b84dfd6c3b88548816889dabf371bfdcf
Lena Davy <79175002880@ya.ru>
2021-10-11 13:32:24 +0300

    Mesto(2)

--- scripts/index.js
+++ scripts/index.js
@@ -1,52 +1,27 @@
 const editButton = document.querySelector('.profile__edit-button');
-
 const profileTitle = document.querySelector('.profile__title');
-
 const profileText = document.querySelector('.profile__text');
-
 const popup = document.querySelector('.popup');
-
-const popupName = document.querySelector('.popup__input_name');
-
-const popupActivity = document.querySelector('.popup__input_activity');
-
-
-editButton.addEventListener('click', function() {
-  popup.classList.add('popup__open');
-  popupName.value = profileTitle.textContent;
-  popupActivity.value = profileText.textContent;
-  }
-);
-
-
+const popupName = document.querySelector('.popup__input_type_name');
+const popupActivity = document.querySelector('.popup__input_type_activity');
 const popupClose = document.querySelector('.popup__close');
-
-function closePopup() {
-  popup.classList.remove('popup__open');
-}
-
-popupClose.addEventListener('click', closePopup);
-
-
-function popupClickHandler(event) {
-  if(event.target.classList.contains('popup')) {
-    closePopup()
-  }
-}
-
-popup.addEventListener('mouseup', popupClickHandler);
-
-
-
-const popupButton = document.querySelector('.popup__button');
-
 const popupForm = document.querySelector('.popup__form');
-
-function formSubmitHandler(event) {
+function closePopup() {
+  popup.classList.remove('popup_open');
+  popup.classList.add('popup');
+};
+function handleFormSubmit(event) {
   event.preventDefault();
   profileTitle.textContent = popupName.value;
   profileText.textContent = popupActivity.value;
   closePopup();
-}
-
-popupForm.addEventListener('submit', formSubmitHandler);
+};
+editButton.addEventListener('click', function() {
+  popup.classList.remove('popup');
+  popup.classList.add('popup_open');
+  popupName.value = profileTitle.textContent;
+  popupActivity.value = profileText.textContent;
+  }
+);
+popupClose.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', handleFormSubmit);
