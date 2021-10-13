7dcb0fa14ec3cf72ce66c2afc83925b6149a816c
Lena Davy <79175002880@ya.ru>
2021-10-13 12:07:54 +0300

    Mesto(4)

--- scripts/index.js
+++ scripts/index.js
@@ -6,20 +6,20 @@ const popupName = document.querySelector('.popup__input_type_name');
 const popupActivity = document.querySelector('.popup__input_type_activity');
 const popupClose = document.querySelector('.popup__close');
 const popupForm = document.querySelector('.popup__form');
-function statusPopup() {
+function togglePopup() {
   popup.classList.toggle('popup_opened');
 };
 function handleFormSubmit(event) {
   event.preventDefault();
   profileTitle.textContent = popupName.value;
   profileText.textContent = popupActivity.value;
-  statusPopup();
+  togglePopup();
 };
 editButton.addEventListener('click', function() {
-  statusPopup();
+  togglePopup();
   popupName.value = profileTitle.textContent;
   popupActivity.value = profileText.textContent;
   }
 );
-popupClose.addEventListener('click', statusPopup);
+popupClose.addEventListener('click', togglePopup);
 popupForm.addEventListener('submit', handleFormSubmit);
